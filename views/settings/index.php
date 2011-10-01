c3f3938e83c6cc436ead712010466a7a5ffe33aa
Brennan Novak <brennan@reverseproductions.com>
2011-10-01 13:23:35 -0700

    commiting latest, js fixes, etc...

--- views/settings/index.php
+++ views/settings/index.php
@@ -3,8 +3,9 @@
 <div class="content_wrap_inner">
 	
 	<div class="content_inner_top_right">
-		<h3>Module</h3>
+		<h3>App</h3>
 		<p><?= form_dropdown('enabled', config_item('enable_disable'), $settings['geoloqi']['enabled']) ?></p>
+		<p><a href="<?= base_url() ?>api/<?= $this_module ?>/uninstall" id="app_uninstall" class="button_delete">Uninstall</a></p>
 	</div>		
 	
 	<h3>Application Keys</h3>
@@ -14,6 +15,8 @@
 	<p><input type="text" name="api_version" value="<?= $settings['geoloqi']['api_version'] ?>"> Api Version</p>			
 				
 	<p><input type="text" name="oauth2_token" value="<?= $settings['geoloqi']['oauth2_token'] ?>"> OAuth Token</p> 
+
+	<input type="hidden" name="module" value="<?= $this_module ?>">
 
 	<p><input type="submit" name="save" value="Save" /></p>
 
